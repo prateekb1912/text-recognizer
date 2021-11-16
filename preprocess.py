35247c3a9f45519ab96d6bf11139c429533cc822
prateek-ml <prateekb1912@gmail.com>
2021-11-16 21:27:35 +0530

    Convert image to grayscale

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,13 @@ with open("annotations.txt", "r") as f:
         file = dict()
         file['filename'] = img_file.strip()
 
+        # Convert the images to grayscale
+        img = Image.open(file['filename']).convert('L')
+        img.save(file['filename'])
+
         folder, label, ext = img_file.split('_')
         file['label'] = label.lower()
+        print(label)
 
         dataset.append(file)
+
