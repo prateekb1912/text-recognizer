9013a58104ce7e02ccf32f282d9431e865830b06
prateek-ml <prateekb1912@gmail.com>
2021-11-16 22:14:42 +0530

    Create model training script

---
+++ model_train.py
@@ -0,0 +1,87 @@
+from keras.layers import Input, Conv2D, MaxPool2D, Dense,MaxPooling2D
+from keras.layers import AveragePooling2D, Flatten, Activation, Bidirectional
+from keras.layers import BatchNormalization, Dropout
+from keras.layers import Concatenate, Add, Multiply, Lambda
+from keras.layers import UpSampling2D, Reshape
+from keras.layers.merge import add,concatenate
+from keras.layers import Reshape
+from keras.models import Model
+from keras.layers.recurrent import LSTM,GRU
+import tensorflow as tf
+
+from utils import *
+
+def Image_text_recogniser_model_1(stage,drop_out_rate=0.35):
+    """
+    Builds the model by taking in the stage variable which specifes the stage,
+    if the stage is training: model takes inputs required for computing ctc_batch_cost function
+    else : model takes input as images which is used for prediction
+    """
+    
+    if K.image_data_format() == 'channels_first':
+        input_shape = (1, img_w, img_h)
+    else:
+        input_shape = (img_w, img_h, 1)
+       
+    model_input=Input(shape=input_shape,name='img_input',dtype='float32')
+
+    # Convolution layer 
+    model = Conv2D(64, (3, 3), padding='same', name='conv1', kernel_initializer='he_normal')(model_input) 
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = MaxPooling2D(pool_size=(2, 2), name='max1')(model) 
+
+    model = Conv2D(128, (3, 3), padding='same', name='conv2', kernel_initializer='he_normal')(model) 
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = MaxPooling2D(pool_size=(2, 2), name='max2')(model) 
+
+    model = Conv2D(256, (3, 3), padding='same', name='conv3', kernel_initializer='he_normal')(model) 
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = Conv2D(256, (3, 3), padding='same', name='conv4', kernel_initializer='he_normal')(model)
+    model=Dropout(drop_out_rate)(model)
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = MaxPooling2D(pool_size=(1, 2), name='max3')(model)  
+
+    model = Conv2D(512, (3, 3), padding='same', name='conv5', kernel_initializer='he_normal')(model) 
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = Conv2D(512, (3, 3), padding='same', name='conv6')(model)
+    model=Dropout(drop_out_rate)(model)
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)
+    model = MaxPooling2D(pool_size=(1, 2), name='max4')(model) 
+
+    model = Conv2D(512, (2, 2), padding='same', kernel_initializer='he_normal', name='con7')(model)
+    model=Dropout(0.25)(model)
+    model = BatchNormalization()(model)
+    model = Activation('relu')(model)    
+
+    # CNN to RNN
+    model = Reshape(target_shape=((42, 1024)), name='reshape')(model)  
+    model = Dense(64, activation='relu', kernel_initializer='he_normal', name='dense1')(model)  
+
+    # RNN layer
+    model=Bidirectional(LSTM(256, return_sequences=True, kernel_initializer='he_normal'), merge_mode='sum')(model)
+    model=Bidirectional(LSTM(256, return_sequences=True, kernel_initializer='he_normal'), merge_mode='concat')(model)
+
+    # transforms RNN output to character activations:
+    model = Dense(num_classes, kernel_initializer='he_normal',name='dense2')(model) 
+    y_pred = Activation('softmax', name='softmax')(model)
+
+    
+    labels = Input(name='ground_truth_labels', shape=[max_length], dtype='float32') 
+    input_length = Input(name='input_length', shape=[1], dtype='int64') 
+    label_length = Input(name='label_length', shape=[1], dtype='int64') 
+
+    #CTC loss function
+    loss_out = Lambda(ctc_loss_function, output_shape=(1,),name='ctc')([y_pred, labels, input_length, label_length]) #(None, 1)
+
+    if stage=='train':
+        return model_input,y_pred,Model(inputs=[model_input, labels, input_length, label_length], outputs=loss_out)
+    else:
+        return Model(inputs=[model_input], outputs=y_pred)  
+
+model_input,y_pred,img_text_recog = Image_text_recogniser_model_1('train')
