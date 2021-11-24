f69acda12e853d6f666480af6e09776c9b8a372e
prateek-ml <prateekb1912@gmail.com>
2021-11-24 23:16:35 +0530

    Finish the model script

--- model_train.py
+++ model_train.py
@@ -2,14 +2,12 @@ from keras import backend as K
 
 from utils import ctc_loss_function, letters
 
-from keras.models import Sequential
 from keras.layers import Input, Conv2D, Dense, MaxPooling2D
 from keras.layers import Activation, Bidirectional
 from keras.layers import BatchNormalization, Dropout
 from keras.layers import Lambda
 from keras.layers import Reshape
-from keras.layers import Reshape
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.layers.recurrent import LSTM
 
 
@@ -32,10 +30,17 @@ class TextRecognizerModel():
             Initializes the model with the image size passed as arguments
         """
         self.input_shape = (img_h, img_w, img_c)
-        self.model = Input(shape=self.input_shape,
-                           name='img_input')
+        self.labels = None
+        self.label_length = 0
+        self.input_length = 0
+        self.loss = None
 
-        self.add_layers()
+        self.model_input = Input(shape=self.input_shape,
+                                 name='img_input')
+
+        self.add_cnn_layers()
+        self.reshape_model()
+        self.add_rnn_layers()
 
     def add_cnn_layers(self):
         """
@@ -43,74 +48,93 @@ class TextRecognizerModel():
             the input image and consisting of convolution layers.
         """
 
-        self.model.add(Conv2D(64, (3, 3),
-                              padding='same', name='conv2d_1'))
-        self.model.add(BatchNormalization())
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), name='max_pool_1'))
+        self.model = Conv2D(64, (3, 3), padding='same', name='conv1',
+                            kernel_initializer='he_normal')(self.model_input)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(2, 2), name='max1')(self.model)
 
-        self.model.add(Conv2D(128, (3, 3),
-                              padding='same', name='conv2d_2'))
-        self.model.add(BatchNormalization())
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), name='max_pool_2'))
+        self.model = Conv2D(128, (3, 3), padding='same', name='conv2',
+                            kernel_initializer='he_normal')(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(2, 2), name='max2')(self.model)
 
-        self.model.add(Conv2D(256, (3, 3), padding='same', name='conv2d_3'))
-        self.model.add(Dropout(0.3))
-        self.model.add(BatchNormalization())
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(1, 2), name='max3'))
+        self.model = Conv2D(256, (3, 3), padding='same', name='conv3',
+                            kernel_initializer='he_normal')(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = Conv2D(256, (3, 3), padding='same', name='conv4',
+                            kernel_initializer='he_normal')(self.model)
+        self.model = Dropout(0.35)(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(1, 2), name='max3')(self.model)
 
-        self.model.add(Conv2D(512, (3, 3),
-                              padding='same', name='conv2d_4'))
-        self.model.add(Dropout(0.3))
-        self.model.add(BatchNormalization())
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), name='max_pool_4'))
+        self.model = Conv2D(512, (3, 3), padding='same', name='conv5',
+                            kernel_initializer='he_normal')(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = Conv2D(512, (3, 3), padding='same',
+                            name='conv6')(self.model)
+        self.model = Dropout(0.35)(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+        self.model = MaxPooling2D(pool_size=(1, 2), name='max4')(self.model)
 
-        self.model.add(Conv2D(512, (3, 3),
-                              padding='same', name='conv2d_5'))
-        self.model.add(Dropout(0.3))
-        self.model.add(BatchNormalization())
-        self.model.add(Activation('relu'))
-        
+        self.model = Conv2D(512, (2, 2), padding='same',
+                            kernel_initializer='he_normal', name='con7')(self.model)
+        self.model = Dropout(0.25)(self.model)
+        self.model = BatchNormalization()(self.model)
+        self.model = Activation('relu')(self.model)
+
     def reshape_model(self):
         """
             Reshape the model to transition from CNN to RNN
         """
-        self.model.add(Reshape(target_shape=((42, 1024)), name='reshape'))
-        self.model.add(Dense(64, activation='relu', name='dense1'))
+        self.model = Reshape(target_shape=((40, 1024)),
+                             name='reshape')(self.model)
+        self.model = Dense(
+            64, activation='relu', kernel_initializer='he_normal', name='dense1')(self.model)
+
+    def add_rnn_layers(self):
+        """
+            Define the RNN layers to go from image to text for predicition
+        """
+        self.model = (Bidirectional(LSTM(256, return_sequences=True,
+                                         kernel_initializer='he_normal'), merge_mode='sum'))(self.model)
+        self.model = (Bidirectional(LSTM(256, return_sequences=True,
+                                         kernel_initializer='he_normal'), merge_mode='sum'))(self.model)
+
+        # Transform the RNN output to character activations
+        # Now, we will be using the functional API as there will be multiple outputs now
+
+        self.model = Dense(num_classes, kernel_initializer='he_normal',
+                           name='dense2')(self.model)
+
+        # Ouput predicted text, take input actual label and their respective lengths for loss
+        # computation
+        self.prediction = Activation('softmax', name='softmax')(self.model)
+
+        self.labels = Input(name='ground_truth_labels', shape=[
+            max_length], dtype='float32')
+        self.input_length = Input(
+            name='input_length', shape=[1], dtype='int64')
+        self.label_length = Input(
+            name='label_length', shape=[1], dtype='int64')
+
+        # CTC loss function
+        self.loss = Lambda(ctc_loss_function, output_shape=(1,), name='ctc')([
+            self.prediction, self.labels,
+            self.input_length, self.label_length])
+
+    def train(self):
+        return self.model_input, self.prediction, Model(inputs=[self.model_input, self.labels,
+                                                                self.input_length, self.label_length], outputs=self.loss)
 
 
-def textRecognizerModel(stage, drop_out_rate=0.35):
+modelObj = TextRecognizerModel(img_h, img_w, img_c)
 
-    # RNN layer
-    model = Bidirectional(LSTM(256, return_sequences=True,
-                          kernel_initializer='he_normal'), merge_mode='sum')(model)
-    model = Bidirectional(LSTM(256, return_sequences=True,
-                          kernel_initializer='he_normal'), merge_mode='concat')(model)
+model_input, prediction, model = modelObj.train()
 
-    # transforms RNN output to character activations:
-    model = Dense(num_classes, kernel_initializer='he_normal',
-                  name='dense2')(model)
-    y_pred = Activation('softmax', name='softmax')(model)
-
-    labels = Input(name='ground_truth_labels', shape=[
-                   max_length], dtype='float32')
-    input_length = Input(name='input_length', shape=[1], dtype='int64')
-    label_length = Input(name='label_length', shape=[1], dtype='int64')
-
-    # CTC loss function
-    loss_out = Lambda(ctc_loss_function, output_shape=(1,), name='ctc')(
-        [y_pred, labels, input_length, label_length])
-
-    if stage == 'train':
-        return model_input, y_pred, Model(inputs=[model_input, labels, input_length, label_length], outputs=loss_out)
-    else:
-        return Model(inputs=[model_input], outputs=y_pred)
-
-
-model_input, y_pred, img_text_recog = textRecognizerModel('train')
-
-test_func = K.function([model_input], [y_pred])
-img_text_recog.summary()
+print(model.summary())
